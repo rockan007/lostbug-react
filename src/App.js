b15b528b9c70bc31776210e7de6a5c2d8741c1b6
Andy An <rockan007@gmail.com>
2022-10-23 09:49:15 +0800

    [feature] add some quote

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App content-img"  style={{backgroundImage:`url(${img})`}}>
+        <p>"The meaning of life lies in the courage to bear the meaninglessness of life."</p>
+        <p>“You will never be happy if you continue to search for what happiness consists of. You will never live if you are looking for the meaning of life.”</p>
+        <p>--Albert Camus</p>
     </div>
   );
 }
